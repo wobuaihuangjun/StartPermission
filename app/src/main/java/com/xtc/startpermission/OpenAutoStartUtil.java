73d3d19a59a84d9715b9b991437a9018b2ef9aeb
huangzhijun <huangzhijun@oaserver.dw.gdbbk.com>
2018-07-10 16:37:46 +0800

    删除默认的跳转

--- app/src/main/java/com/xtc/startpermission/OpenAutoStartUtil.java
+++ app/src/main/java/com/xtc/startpermission/OpenAutoStartUtil.java
@@ -3,8 +3,6 @@ package com.xtc.startpermission;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
 import android.util.Log;
 
 
@@ -60,8 +58,6 @@ public class OpenAutoStartUtil {
     /**
      * GoTo Open Self Setting Layout
      * Compatible Mainstream Models 兼容市面主流机型
-     *
-     * @param context
      */
     public static void jumpStartInterface(Context context) {
         String brand = DeviceUtil.getPhoneBrand().toLowerCase();
@@ -96,48 +92,5 @@ public class OpenAutoStartUtil {
             }
         }
 
-
-        try {
-            ComponentName componentName = null;
-            Intent intent = new Intent();
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-            if (product.equals("Letv")) { // 乐视2测试通过
-                intent.setAction("com.letv.android.permissionautoboot");
-
-            } else if (product.equals("OPPO")) { // OPPO R8205测试通过
-                componentName = ComponentName.unflattenFromString("com.oppo.safe" +
-                        "/.permission.startup.StartupAppListActivity");
-                Intent intentOppo = new Intent();
-                intentOppo.setClassName("com.oppo.safe/.permission.startup",
-                        "StartupAppListActivity");
-                if (context.getPackageManager().resolveActivity(intentOppo, 0) == null) {
-                    componentName = ComponentName.unflattenFromString("com.coloros.safecenter" +
-                            "/.startupapp.StartupAppListActivity");
-                }
-
-            } else if (product.equals("ulong")) { // 360手机 未测试
-                componentName = new ComponentName("com.yulong.android.coolsafe",
-                        ".ui.activity.autorun.AutoRunListActivity");
-            } else {
-                // 以上只是市面上主流机型，由于公司你懂的，所以很不容易才凑齐以上设备
-                // 针对于其他设备，我们只能调整当前系统app查看详情界面
-                // 在此根据用户手机当前版本跳转系统设置界面
-                if (Build.VERSION.SDK_INT >= 9) {
-                    intent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
-                    intent.setData(Uri.fromParts("package", context.getPackageName(), null));
-                } else if (Build.VERSION.SDK_INT <= 8) {
-                    intent.setAction(Intent.ACTION_VIEW);
-                    intent.setClassName("com.android.settings",
-                            "com.android.settings.InstalledAppDetails");
-                    intent.putExtra("com.android.settings.ApplicationPkgName",
-                            context.getPackageName());
-                }
-            }
-            intent.setComponent(componentName);
-            context.startActivity(intent);
-        } catch (Exception e) {//抛出异常就直接打开设置页面
-            e.printStackTrace();
-        }
     }
 }
